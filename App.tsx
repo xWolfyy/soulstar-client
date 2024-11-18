7fb8a3f56d952647e81770445b3925b7ebd65ad2
mauro.lambrecht05@gmail.com <mauro.lambrecht05@gmail.com>
2024-11-18 19:05:35 +0100

    removed android, and edited files

--- App.tsx
+++ App.tsx
@@ -1,46 +1,12 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import {
-  ViroARScene,
-  ViroText,
-  ViroTrackingStateConstants,
-  ViroARSceneNavigator,
-  ViroTrackingReason,
-} from "@reactvision/react-viro";
-
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState("Initializing AR...");
-
-  function onInitialized(state: any, reason: ViroTrackingReason) {
-    console.log("guncelleme", state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText("Hello World!");
-    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
-    }
-  }
-
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
-  );
-};
+import { Text, View } from "react-native";
 
 export default () => {
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
+    <View style={[styles.f1, { backgroundColor: "#000000" }]}>
+      <Text style={styles.helloWorldTextStyle}>Hello World</Text>
+    </View>
   );
 };
 
