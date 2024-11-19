f2fef891d65b06c7f9948260a3d131e041a1cd68
mauro.lambrecht05@gmail.com <mauro.lambrecht05@gmail.com>
2024-11-19 14:36:00 +0100

    k

--- android/app/src/main/java/com/wolfy/soulstar/MainApplication.kt
+++ android/app/src/main/java/com/wolfy/soulstar/MainApplication.kt
@@ -1,50 +1,61 @@
+
+
 package com.wolfy.soulstar
+import com.viromedia.bridge.ReactViroPackage
 
 import android.app.Application
 import android.content.res.Configuration
+
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-import com.viromedia.bridge.ReactViroPackage
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> {
-                val packages = PackageList(this).packages
-                // Manually add ReactViroPackage
-                packages.add(ReactViroPackage(ReactViroPackage.ViroPlatform.AR))
-                return packages
-            }
+          override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            packages.add(ReactViroPackage(ReactViroPackage.ViroPlatform.AR))
+            return packages
+          }
 
-            override fun getJSMainModuleName(): String = "index" // Default entry point
+          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-        }
-    )
+          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+  )
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, false) // Correct initialization
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            load() // Load the native entry point if new architecture is enabled
-        }
-        ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  override val reactHost: ReactHost
+    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, OpenSourceMergedSoMapping)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
     }
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-    }
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
 }
